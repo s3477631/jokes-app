6812826c96b947dcd47328306a897bdcc158d4ad
devin <devin@nukon.com>
2021-04-06 22:03:33 +1000

    first commit

---
+++ src/test/java/ordered/springboot/jokes/jokesapp/JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ordered.springboot.jokes.jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
